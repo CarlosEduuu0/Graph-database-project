ea3ff8f26c022a1c232504573568f1e30b9a380a
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-09-12 17:58:37 -0300

    feat: adicionando outras funcoes ao menu

--- menu.py
+++ menu.py
@@ -9,6 +9,7 @@ def menu():
         if user_input == "sair":
             break
         elif user_input == "1":
+            # TRATAR ERROS NO INTERPRETADOR
 
             vertex_name = input("digite o nome do vertice\n")
             properties_size = int(input("quantas propriedades terá o seu vertice?\n"))
@@ -20,20 +21,53 @@ def menu():
             graph.addVertex(name= vertex_name, properties=vertex_properties)
 
         elif user_input == "2":
+            #TRATAR ERROS NO INTERPRETADOR
 
             from_vertex = input("de qual vertice vira a conexao\n")
             to_vertex = input("qual vertice recebera a conexao\n")
             relation_name = input("qual sera o nome da relacao\n")
             graph.connectVertices(relation_name=relation_name, from_vertex=from_vertex, to_vertex=to_vertex)
         elif user_input == "3":
-            print("funcao nao feita ainda")
+            #TRATAR ERROS NO INTERPRETADOR
+
+            vertex_name = input("digite o nome do vertice a ser atualizado\n")
+            choice = input("digite 1 para atualizar o nome do vertice")
+            if choice == "1":
+                choice = 0
+                vertex_newName = input("digite o novo nome do vertice\n")
+
+            choice = input("digite 1 para atualizar as propriedades do vertiec")
+            if choice == "1":
+                properties_size = int(input("quantas propriedades terá o seu vertice?\n"))
+                vertex_newProperties = {}
+                for i in range(properties_size):
+                    key = input("digite o nome da propriedade\n")
+                    value = input("digite o valor da propriedade\n")
+                    vertex_properties[key] = value
+            
+            graph.updateVertex(name=vertex_name, new_name=vertex_newName, new_properties= vertex_newProperties)
+
         elif user_input == "4":
-            print("funcao nao feita ainda")
+            #TRATAR ERROS NO INTERPRETADOR
+
+            vertex_name = input("digite o nome do vertice a ser apagado")
+            graph.deleteVertex(vertex_name)
         elif user_input == "5":
-            print("funcao nao feita ainda")
+            #TRATAR ERROS NO INTERPRETADOR
+
+            from_vertex = input("de qual vertice vem a conexao\n")
+            to_vertex = input("qual vertice recebe a conexao\n")
+            new_relation_name = input("qual sera o novo nome da relacao\n")
+            graph.updateRelation(from_vertex=from_vertex, to_vertex=to_vertex, relation_name = new_relation_name)
         elif user_input == "6":
-            print("funcao nao feita ainda")
+            #TRATAR ERROS NO INTERPRETADOR
+
+            from_vertex = input("de qual vertice vem a conexao\n")
+            to_vertex = input("qual vertice recebe a conexao\n")
+            graph.disconnect(from_vertex=from_vertex, to_vertex=to_vertex)
         elif user_input == "7":
+            #TRATAR ERROS NO INTERPRETADOR
+            
             graph.showGraph()
 
 if __name__ == "__main__":
